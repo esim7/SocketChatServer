2cc2c56193347df7747f4f10ec47bcf581a7ddb5
esim7 <makishev@yahoo.com>
2020-01-08 17:13:40 +0600

    загрузил серверную часть

---
+++ SocketChatServer/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketChatServer
+{
+    public class Message
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string From { get; set; }
+        public DateTime SendTime { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ SocketChatServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketChatServer
+{
+    class Program
+    {
+        static int port = 8005; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            List<Message> Messages = new List<Message>();
+
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+
+                listenSocket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; 
+                    byte[] data = new byte[256]; 
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    var message = JsonConvert.DeserializeObject<Message>(builder.ToString());
+                    Messages.Add(message);
+
+                    // отправляем ответ
+                    var answerData = JsonConvert.SerializeObject(Messages);
+                    data = Encoding.Unicode.GetBytes(answerData);
+                    handler.Send(data);
+                    // закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
