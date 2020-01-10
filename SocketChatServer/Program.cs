39256f89bc707af1e478ceb17e1104568d8d0789
esim7 <makishev@yahoo.com>
2020-01-10 17:55:43 +0600

    обновил

--- SocketChatServer/Program.cs
+++ SocketChatServer/Program.cs
@@ -4,60 +4,69 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SocketChatServer
 {
     class Program
     {
-        static int port = 8005; // порт для приема входящих запросов
+        static int port = 3231;
         static void Main(string[] args)
         {
-            List<Message> Messages = new List<Message>();
+            StartChatServerAsync();
 
-            // получаем адреса для запуска сокета
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            string s = Console.ReadLine();
+        }
 
-            // создаем сокет
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
+        private static async void StartChatServerAsync()
+        {
+            await Task.Run(() =>
             {
-                // связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(ipPoint);
-
-                listenSocket.Listen(10);
-                Console.WriteLine("Сервер запущен. Ожидание подключений...");
-
-                while (true)
+                List<Message> Messages = new List<Message>();
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
                 {
-                    Socket handler = listenSocket.Accept();
-                    // получаем сообщение
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0; 
-                    byte[] data = new byte[256]; 
-
-                    do
+                    listenSocket.Bind(ipPoint);
+                    listenSocket.Listen(10);
+                    Console.WriteLine("Сервер запущен. Ожидание подключений...");
+                    while (true)
                     {
-                        bytes = handler.Receive(data);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        Socket handler = listenSocket.Accept();
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0;
+                        byte[] data = new byte[256];
+                        do
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        while (handler.Available > 0);
+                        var message = JsonConvert.DeserializeObject<Message>(builder.ToString());
+
+                        if (message.Text != string.Empty)
+                        {
+                            Messages.Add(message);
+                            using (var context = new ChatContext())
+                            {
+                                context.Messages.Add(message);
+                                context.SaveChanges(); 
+                            }
+                        }
+                        var answerData = JsonConvert.SerializeObject(Messages);
+                        data = Encoding.Unicode.GetBytes(answerData);
+                        handler.Send(data);
+
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
                     }
-                    while (handler.Available > 0);
-
-                    var message = JsonConvert.DeserializeObject<Message>(builder.ToString());
-                    Messages.Add(message);
-
-                    // отправляем ответ
-                    var answerData = JsonConvert.SerializeObject(Messages);
-                    data = Encoding.Unicode.GetBytes(answerData);
-                    handler.Send(data);
-                    // закрываем сокет
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+            
         }
     }
 }
